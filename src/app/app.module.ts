49892d84ad915d3eb77a456c7d5c40193df93dae
Kirill <kirarito.kira@gmail.com>
2018-08-07 16:26:54 +0300

    Initial commit. Troubles with adding angular-material-search component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { NoopAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
+
+import {MatInputModule} from '@angular/material';
+
+
 import { AppComponent } from './app.component';
+import { ClientsListComponent } from './clients-list/clients-list.component';
+import { ClientItemComponent } from './client-item/client-item.component';
+import { SearchClientInputComponent } from './search-client-input/search-client-input.component';
+import { ClientInfoComponent } from './client-info/client-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientsListComponent,
+    ClientItemComponent,
+    SearchClientInputComponent,
+    ClientInfoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    NoopAnimationsModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/search-client-input/search-client-input.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, NgModule } from '@angular/core';
+
+
+@Component({
+  selector: 'app-search-client-input',
+  templateUrl: './search-client-input.component.html',
+  styleUrls: ['./search-client-input.component.css']
+})
+
+export class SearchClientInputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
